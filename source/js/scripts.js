e7e969d30c5bd4d2b332e8f9c9183c04c56b7741
quoc <bui.vquoc@gmail.com>
2019-11-18 15:46:03 +0700

    add The IntersectionObserver

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,22 +3,40 @@
 
 import Util from './utils/Util';
 // import Resizer from "./utils/Resizer";
-
 import barba from '@barba/core';
 import barbaPrefetch from '@barba/prefetch';
 import barbaCss from '@barba/css';
-
 import imagesLoaded from 'imagesloaded';
 
-// tell Barba to use the prefetch module
-barba.use(barbaPrefetch);
+// Selector all headers in pages
+const headers = document.querySelectorAll('h2, h3');
 
-// tell Barba to use the css module
-barba.use(barbaCss);
-
-// init Barba
-barba.init({
-  views: [],
-  transitions: [],
+// Connect headers to the observer
+// Has two agruments:
+// - callback => check every single time this changes over a certain treshold
+// - options => Options that is passed into IO() constructor to control the circumstances under which the observer's callback is invoked
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // if this entry has an intersection over or equal 10% treshhold (10% page visisble) add an class else
+    if (entry.intersectionRatio >= 0.1) {
+      entry.target.classList.add('in-view');
+    } else {
+      entry.target.classList.remove('in-view'); // remove the classs if the page visible is lesser then 10%
+    };
+  })
+}, {
+  threshold: [0, 0.1, 1], // Listen at 0, 10% and 100% percentage of the target's visibility the observer's callback should be execute
 });
+
+// observer has to make an callback on each each element
+headers.forEach((header) => {
+  observer.observe(header)
+})
+
+// barba.use(barbaPrefetch);
+// barba.use(barbaCss);
+// barba.init({
+//   views: [],
+//   transitions: [],
+// });
 
